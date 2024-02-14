c187ff57b2f446c48d6af96c9f78de6046337264
Vinicius Muller <vinicius.muller20@outlook.com>
2024-02-14 19:10:00 -0300

    Post return by id

--- src/main/java/com/viniciusmuller/workshopmongo/resources/UserResource.java
+++ src/main/java/com/viniciusmuller/workshopmongo/resources/UserResource.java
@@ -16,6 +16,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
+import com.viniciusmuller.workshopmongo.domain.Post;
 import com.viniciusmuller.workshopmongo.domain.User;
 import com.viniciusmuller.workshopmongo.dto.UserDTO;
 import com.viniciusmuller.workshopmongo.services.UserService;
@@ -65,6 +66,13 @@ public class UserResource {
 		obj = service.update(obj);
 		return ResponseEntity.noContent().build();
 		
+	}
+	
+	@GetMapping(value = "/{id}/posts")
+	public ResponseEntity<List<Post>> findPosts(@PathVariable String id){
+		
+		User obj = service.findById(id);
+		return ResponseEntity.ok().body(obj.getPosts());
 	}
 	
 }
