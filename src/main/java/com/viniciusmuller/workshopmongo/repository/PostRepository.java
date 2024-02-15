88693106ecabae343ba985b61c8156d3fa19ac98
Vinicius Muller <vinicius.muller20@outlook.com>
2024-02-14 21:53:03 -0300

    Search using @Query

--- src/main/java/com/viniciusmuller/workshopmongo/repository/PostRepository.java
+++ src/main/java/com/viniciusmuller/workshopmongo/repository/PostRepository.java
@@ -3,6 +3,7 @@ package com.viniciusmuller.workshopmongo.repository;
 import java.util.List;
 
 import org.springframework.data.mongodb.repository.MongoRepository;
+import org.springframework.data.mongodb.repository.Query;
 import org.springframework.stereotype.Repository;
 
 import com.viniciusmuller.workshopmongo.domain.Post;
@@ -10,6 +11,9 @@ import com.viniciusmuller.workshopmongo.domain.Post;
 @Repository
 public interface PostRepository extends MongoRepository<Post, String>{
 
+		@Query("{ 'title': { $regex: ?0, /pattern/, $options: 'i' } }")
+		List<Post> searchTitle(String text);
+		
 		List<Post> findByTitleContainingIgnoreCase(String text);
 	
 }
